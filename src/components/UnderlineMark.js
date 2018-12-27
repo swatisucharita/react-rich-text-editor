3eb0e266222a3fd7e8f9e413b25e3397041ead4d
Sucharita <sucsw01@ca.com>
2018-12-27 13:02:48 +0530

    FIx styled component

--- src/components/UnderlineMark.js
+++ src/components/UnderlineMark.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const styledSpan = styled.span `
-    textDecoration: 'underline
+const StyledSpan = styled.span`
+    text-decoration: underline;
 `
 const UnderlineMark = (props) => {
     return (
-        <styledSpan>{props.children}</styledSpan>
+        <StyledSpan>{props.children}</StyledSpan>
     )
 }
 
